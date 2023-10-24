6ef1b59891f139e4f3696a64b712b489824f4c91
André Fonseca Borges <andre.borges@demarco.com.br>
2023-10-24 09:17:01 -0300

    up Code

--- api/models/curso_model.py
+++ api/models/curso_model.py
@@ -1,12 +1,16 @@
 from api import db
+from ..models import formacao_model
 
 
 # criar tabela
 class Curso(db.Model):
     __tablename__ = 'curso'
-    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True,
                    autoincrement=True, nullable=False)
     nome = db.Column(db.String(50), nullable=False)
     descricao = db.Column(db.String(100), nullable=False)
     data_publicacao = db.Column(db.Date, nullable=False)
+
+    formacao_id = db.Column(db.Integer, db.ForeignKey("formacao.id"))
+    formcao = db.relationship(formacao_model.Formacao, backref=db.backref("cursos",
+                                                                          lazy="dynamic"))
--- api/services/curso_service.py
+++ api/services/curso_service.py
@@ -5,7 +5,8 @@ from api import db
 def cadatrar_curso(curso):
     curso_bd = curso_model.Curso(nome=curso.nome,
                                  descricao=curso.descricao,
-                                 data_publicacao=curso.data_publicacao)
+                                 data_publicacao=curso.data_publicacao,
+                                 formacao=curso.formacao)
     db.session.add(curso_bd)
     db.session.commit()
     return curso_bd
@@ -25,6 +26,7 @@ def atualiza_curso(curso_anterior, curso_novo):
     curso_anterior.nome = curso_novo.nome
     curso_anterior.descricao = curso_novo.descricao
     curso_anterior.data_publicacao = curso_novo.data_publicacao
+    curso_anterior.formacao = curso_novo.formacao
     db.session.commit()
     return "Curso atualizado com sucesso"
 
