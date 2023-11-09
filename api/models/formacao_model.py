d36b9cb1e0bb50c126afec327138d1b46ed931ab
André Fonseca Borges <andre.borges@demarco.com.br>
2023-11-09 07:13:21 -0300

    up Code

--- api/models/formacao_model.py
+++ api/models/formacao_model.py
@@ -1,4 +1,14 @@
 from api import db
+from .professor_model import Professor
+
+# Criação da tabela que vai fazer a ligação entre professor e formacao
+professor_formcao = db.Table('professor_formacao',
+                             db.Column('professor_id', db.Integer,
+                                       db.ForeignKey('professor.id'),
+                                       primary_key=True, nullable=False),
+                             db.Column('formacao_id', db.Integer,
+                                       db.ForeignKey('formacao.id'),
+                                       primary_key=True, nullable=False))
 
 
 # criar tabela
@@ -8,3 +18,6 @@ class Formacao(db.Model):
                    autoincrement=True, nullable=False)
     nome = db.Column(db.String(50), nullable=False)
     descricao = db.Column(db.String(100), nullable=False)
+    professores = db.relationship(Professor,
+                                  secondary='professor_formacao',  # Faz a ligação com a nova tabela
+                                  back_populates='formacoes')  # Onde os dados vão ser puxados
--- api/models/professor_model.py
+++ api/models/professor_model.py
@@ -9,3 +9,6 @@ class Professor(db.Model):
     nome = db.Column(db.String(50), nullable=False)
     idade = db.Column(db.Integer, nullable=False)
     titulo = db.Column(db.String(50), nullable=True)
+    formacoes = db.relationship("Formacao",  # Como foi criado a relação dentro do formacao_model, fica assim
+                                secondary='professor_formacao',  # Faz a ligação com a nova tabela
+                                back_populates='professores')  # Onde os dados vão ser puxados
