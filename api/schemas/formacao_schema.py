98a26c89b1c050628acaa0365dd87e5f225e85aa
André Fonseca Borges <andre.borges@demarco.com.br>
2023-11-10 18:21:33 -0300

    up Code

--- api/schemas/formacao_schema.py
+++ api/schemas/formacao_schema.py
@@ -1,10 +1,12 @@
 from api import ma
 from ..models import formacao_model
 from marshmallow import fields
-from ..schemas import curso_schema
+from ..schemas import curso_schema, professor_schema
 
 
 class FormacaoSchema(ma.SQLAlchemyAutoSchema):
+    professores = ma.Nested(professor_schema.ProfessorSchema, many=True)  # Retorna os dados de professores
+
     class Meta:
         model = formacao_model.Formacao
         load_instance = True
