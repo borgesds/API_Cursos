e3d533433f462ecfc0b64161394e77f78e2661c2
André Fonseca Borges <andre.borges@demarco.com.br>
2023-11-17 09:22:48 -0300

    up Code

---
+++ api/schemas/login_schema.py
@@ -0,0 +1,14 @@
+from api import ma
+from ..models import usuario_model
+from marshmallow import fields
+
+
+class LoginSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = usuario_model.Usuario
+        load_instance = True
+        fields = ('id', 'nome', 'email', 'senha')
+
+    nome = fields.String(required=False)  # Nome não vai ser obrigatorio
+    email = fields.String(required=True)
+    senha = fields.String(required=True)
--- api/services/usuario_service.py
+++ api/services/usuario_service.py
@@ -12,3 +12,8 @@ def cadatrar_usuario(usuario):
     db.session.add(usuario_bd)
     db.session.commit()
     return usuario_bd
+
+
+# Vamos listar os email para saber se tem login
+def listar_usuario_email(email):
+    return usuario_model.Usuario.query.filter_by(email=email).first()
